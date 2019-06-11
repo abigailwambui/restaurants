55f8d1088c8607496b5557ca2c127c5541c70be8
abigailwambui <abigailw15njuguna@gmail.com>
2019-06-11 10:32:30 +0300

    Include intent, TextEditView, button, acivities, toast to my restaurant app

---
+++ app/src/main/java/com/example/myresturant/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.myresturant;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+
+
+public class MainActivity extends AppCompatActivity {
+    private Button mFindRestaurantsButton;
+    private EditText mLocationEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mLocationEditText = (EditText) findViewById(R.id.locationEditText);
+        mFindRestaurantsButton = (Button) findViewById(R.id.findRestaurantsButton);
+        mFindRestaurantsButton.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    String location = mLocationEditText.getText().toString();
+                    Intent intent =  new Intent(MainActivity.this, ResturantsActivity.class);
+                    intent.putExtra("location", location);
+                    startActivity(intent);
+                }
+            });
+
+
+
+
+    }
+}
